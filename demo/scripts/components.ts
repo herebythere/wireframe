0ae758f4b611d292dba45744a21ea95dfe3b1399
Taylor Vann <brian.t.vann@gmail.com>
2026-03-11 01:27:09 -0700

    add components to demo, add another theme

---
+++ demo/scripts/components.ts
@@ -0,0 +1,3 @@
+import { InlineMovement } from "../../components/dist/inline_movement.js";
+
+window.customElements.define("inline-movement", InlineMovement);
---
+++ demo/scripts/hangar.ts
@@ -0,0 +1,7 @@
+// Repeat svg with element in the center.
+// Or aligned with text?
+
+export class Hangar extends HTMLElement {
+
+}
+
