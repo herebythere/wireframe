6e6e843f5067c634787ba822fecda9af7aa01579
Taylor Vann <brian.t.vann@gmail.com>
2026-03-20 00:39:41 -0700

    slotted changes

--- components/src/inline_movement.ts
+++ components/src/inline_movement.ts
@@ -13,34 +13,13 @@ export const template = `<template>
 let templateEl = document.createElement("template");
 templateEl.setHTMLUnsafe(shadowDom);
 
-function getSlotElement(el: HTMLElement): HTMLSlotElement | null {
-	let internals = el.attachInternals();
-	let ssr = null !== internals.shadowRoot;
-	let shadowRoot = internals.shadowRoot
-		? internals.shadowRoot
-		: el.attachShadow({ mode: "closed" });
-
-	if (!ssr) {
-		console.log("NO SSR!");
-		shadowRoot.appendChild(document.importNode(templateEl.content, true));
-	}
-
-	return shadowRoot.querySelector("slot");
-}
-
 export class InlineMovement extends HTMLElement {
 	#boundOnSlotChange = this.#onSlotChange.bind(this);
 	#boundOnClick = this.#onClick.bind(this);
 	#boundOnKey = this.#onKey.bind(this);
 	#computedStyle = window.getComputedStyle(this);
 	#slot = getSlotElement(this);
-
-	#slotted = new Set<Element>(this.#slot?.assignedElements() ?? []);
-
-	constructor() {
-		super();
-		console.log(this.#slotted);
-	}
+	#mapped = new WeakSet<EventTarget>(this.#slot?.assignedElements() ?? []);
 
 	connectedCallback() {
 		this.#slot?.addEventListener("slotchange", this.#boundOnSlotChange);
@@ -55,80 +34,84 @@ export class InlineMovement extends HTMLElement {
 	}
 
 	#onSlotChange(_event: Event) {
-		this.#slotted = new Set(this.#slot?.assignedElements() ?? []);
+		this.#mapped = new Set(this.#slot?.assignedElements() ?? []);
 	}
 
 	#onKey(event: KeyboardEvent) {
-		if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-			for (let node of event.composedPath()) {
-				if (node === event.currentTarget) return;
+		if (!this.#slot) return;
+		if (event.defaultPrevented) return;
+		if (event.shiftKey) return;
+		if (event.key !== "ArrowLeft" && event.key !== "ArrowRight") return
 
-				if (node instanceof HTMLElement && this.#slotted.has(node)) {
-					let sibling = getSibling(event, node, this.#computedStyle.direction);
-					if (sibling) {
-						for (let el of this.#slotted) {
-							el.setAttribute("tabindex", "-1");
-						}
-						sibling.setAttribute("tabindex", "0")
-						sibling.focus();
-					}
+		for (let node of event.composedPath()) {
+			if (node === event.currentTarget) return;
+			if (!this.#mapped.has(node)) continue;
 
-					return;
-				};
-
+			let sibling = getSibling(event, node, this.#computedStyle);
+			if (sibling instanceof HTMLElement) {
+				event.preventDefault();
+				setNegativeTabIndices(this.#slot);
+				focusOnElement(sibling);
 			}
+			return;
 		}
 	}
 
 	#onClick(event: PointerEvent) {
 		for (let node of event.composedPath()) {
+			if (node === event.currentTarget) return;
+
+			if (!this.#mapped.has(node)) continue;
 			if (!(node instanceof HTMLElement)) continue;
-			if (!this.#slotted.has(node)) continue;
 
-			for (let el of this.#slotted) {
-				el.setAttribute("tabindex", "-1");
-			}
-
-			node.setAttribute("tabindex", "0");
-			node.focus();
-
+			event.preventDefault();
+			setNegativeTabIndices(this.#slot);
+			focusOnElement(node);
 			return;
 		};
 	}
 }
 
-function getSibling(event: KeyboardEvent, node: HTMLElement, direction: string): HTMLElement | undefined {
-	// do something
-	/*
-		ltr
-		left - prevElementSibling
-		right - nextElementSibling
-	
-		rtl
-		left - nextElementSibling
-		right - prevElementSibling
-	*/
+function getSlotElement(el: HTMLElement): HTMLSlotElement | null {
+	let internals = el.attachInternals();
+	let ssr = null !== internals.shadowRoot;
+	let shadowRoot = internals.shadowRoot
+		? internals.shadowRoot
+		: el.attachShadow({ mode: "closed" });
+
+	if (!ssr)
+		shadowRoot.appendChild(document.importNode(templateEl.content, true));
+
+	return shadowRoot.querySelector("slot");
+}
+
+function setNegativeTabIndices(slot: HTMLSlotElement | null) {
+	if (!slot) return;
+
+	for (let el of slot.assignedElements()) {
+		el.setAttribute("tabindex", "-1");
+	}
+}
+
+function focusOnElement(sibling: HTMLElement) {
+	sibling.setAttribute("tabindex", "0")
+	sibling.focus();
+}
+
+function getSibling(event: KeyboardEvent, node: EventTarget, computedStyle: CSSStyleDeclaration): Element | null | undefined {
+	if (!(node instanceof HTMLElement)) return;
 
 	let prev = node.previousElementSibling;
 	let next = node.nextElementSibling;
+	let { direction } = computedStyle;
 
 	if ("ltr" === direction) {
-		if ("ArrowLeft" === event.key) {
-			if (prev instanceof HTMLElement) {
-				return prev;
-			}
-		}
-		if ("ArrowRight" === event.key) {
-			if (next instanceof HTMLElement) return next;
-		}
+		if ("ArrowLeft" === event.key) return prev;
+		if ("ArrowRight" === event.key) return next;
 	}
 
 	if ("rtl" === direction) {
-		if ("ArrowRight" === event.key) {
-			if (prev instanceof HTMLElement) return prev;
-		}
-		if ("ArrowLeft" === event.key) {
-			if (next instanceof HTMLElement) return next;
-		}
+		if ("ArrowRight" === event.key) return prev;
+		if ("ArrowLeft" === event.key) return next;
 	}
 }
