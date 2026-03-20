cded0ad6c601be1c2a897a89e366e71f6cf09ee3
Taylor Vann <brian.t.vann@gmail.com>
2026-03-19 18:35:56 -0700

    inline movement moves focus inline

--- components/src/inline_movement.ts
+++ components/src/inline_movement.ts
@@ -11,16 +11,15 @@ export const template = `<template>
 <template>`;
 
 let templateEl = document.createElement("template");
-templateEl.setHTMLUnsafe(template);
-
+templateEl.setHTMLUnsafe(shadowDom);
 
 function getSlotElement(el: HTMLElement): HTMLSlotElement | null {
 	let internals = el.attachInternals();
 	let ssr = null !== internals.shadowRoot;
-	let shadowRoot = internals.shadowRoot 
+	let shadowRoot = internals.shadowRoot
 		? internals.shadowRoot
 		: el.attachShadow({ mode: "closed" });
-	
+
 	if (!ssr) {
 		console.log("NO SSR!");
 		shadowRoot.appendChild(document.importNode(templateEl.content, true));
@@ -60,76 +59,73 @@ export class InlineMovement extends HTMLElement {
 	}
 
 	#onKey(event: KeyboardEvent) {
-		console.log(event);
-		if ("ArrowUp" === event.key) { }
-		if ("ArrowRight" === event.key) { }
-		if ("ArrowDown" === event.key) { }
-		if ("ArrowLeft" === event.key) { }
-	
-		if ("rtl" === this.#computedStyle.direction) {
-			// reverse direction
-		}
+		if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+			for (let node of event.composedPath()) {
+				if (node === event.currentTarget) return;
 
-		for (let node of event.composedPath()) {
-			if (node === event.currentTarget) return;
-			if (node instanceof Element && this.#slotted.has(node)) {
-				// do something
-				/*
-					ltr
-					left - prevElementSibling
-					right - nextElementSibling
-				
-					rtl
-					left - nextElementSibling
-					right - prevElementSibling
-				*/
-				node.nextElementSibling;
-				node.previousElementSibling;
-			};
+				if (node instanceof HTMLElement && this.#slotted.has(node)) {
+					let sibling = getSibling(event, node, this.#computedStyle.direction);
+					if (sibling) {
+						for (let el of this.#slotted) {
+							el.setAttribute("tabindex", "-1");
+						}
+						sibling.setAttribute("tabindex", "0")
+						sibling.focus();
+					}
+
+					return;
+				};
+
+			}
 		}
 	}
 
 	#onClick(event: PointerEvent) {
-		console.log(event);
-
 		for (let node of event.composedPath()) {
 			if (!(node instanceof HTMLElement)) continue;
 			if (!this.#slotted.has(node)) continue;
 
-			// check if active element exists in slotted
-			if (
-				document.activeElement && this.#slotted.has(document.activeElement)) {
-				node.setAttribute("tabindex", "-1");
+			for (let el of this.#slotted) {
+				el.setAttribute("tabindex", "-1");
 			}
 
 			node.setAttribute("tabindex", "0");
 			node.focus();
+
+			return;
 		};
 	}
 }
 
-// function getAssignedNodes(slot: HTMLSlotElement | null, selector: string | null): Element[] {
-// 	if (!slot) return [];
-// 	if (!selector) return slot.assignedElements();
+function getSibling(event: KeyboardEvent, node: HTMLElement, direction: string): HTMLElement | undefined {
+	// do something
+	/*
+		ltr
+		left - prevElementSibling
+		right - nextElementSibling
+	
+		rtl
+		left - nextElementSibling
+		right - prevElementSibling
+	*/
 
-// 	// let elements: Element[] = [];
-// 	// for (let el of slot.assignedElements()) {
-// 	// 	if (el.matches(selector)) {
-// 	// 		elements.push(el);
-// 	// 	}
-// 	// }
-
-// 	// return elements;
-// }
-
-function getMappedElements(slot: HTMLSlotElement | null): WeakMap<Element, number> {
-	let elements = slot?.assignedElements() ?? [];
-	let map = new WeakMap();
-
-
-	for (const [index, el] of elements.entries()) {
-		map.set(el, index);
+	if ("ltr" === direction) {
+		if ("ArrowLeft" === event.key) {
+			if (node.previousElementSibling instanceof HTMLElement) {
+				return node.previousElementSibling;
+			}
+		}
+		if ("ArrowRight" === event.key) {
+			if (node.nextElementSibling instanceof HTMLElement) return node.nextElementSibling;
+		}
 	}
 
-	return map;
+	if ("rtl" === direction) {
+		if ("ArrowRight" === event.key) {
+			if (node.previousElementSibling instanceof HTMLElement) return node.previousElementSibling;
+		}
+		if ("ArrowLeft" === event.key) {
+			if (node.nextElementSibling instanceof HTMLElement) return node.nextElementSibling;
+		}
+	}
 }
